14c72a9b0c9d08cb07a437871ca8525d4ad3065d
Paras Ahuja <plkl.bffz@gmail.com>
2026-08-06 23:48:32 +0530

    thirdlevelstarts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 First Level - Arrays & Strings: Two Sum / Three Sum variations , Trapping Rain Water , Valid Parentheses ,Longest Substring Without Repeating Characters , Find Peak Element ,Product of Array Except Self
+-----
+
 Second Level - Binary Tree Level Order Traversal, Maximum Depth of Binary Tree , Validate Binary Search Tree , Lowest Common Ancestor,  Number of Islands
+
+-----
+
+Third Level - Climbing Stairs Maximum Subarray (Kadane's Algorithm) Best Time to Buy and Sell Stock House Robber
---
+++ Third_level/climbingstairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #tabulization solution
+        #meaning bottom to top approach where smallest is calculated first
+        #smallest is given
+        if n<=1:
+            return 1
+        result=0
+        prev1= 1 #0th element
+        prev2= 1 #1st element
+        #basically fn= fn-1 + fn-2
+        for i in range(2,n+1):
+            result=prev1+prev2
+            #make last as second last #0th
+            prev1=prev2
+            #make secondlast as last
+            prev2=result
+        return result
+
+sol=Solution()
+print(sol.climbStairs(2))
